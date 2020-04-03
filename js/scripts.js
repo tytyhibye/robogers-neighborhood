63cb18214052fdeb391a4846693338aec09b8d13
Tyler Bates <Fondlytyler@gmail.com>
2020-04-03 16:00:24 -0700

    further styling and alignment of page content.

--- js/scripts.js
+++ js/scripts.js
@@ -17,7 +17,7 @@ $(document).ready(function() {
     num = $("input#number").val();
     for(i = 0; i <= num; i++) {
       if(i.toString().includes(3)) {
-       stringHolder.push("Won't you be my neighbor")
+       stringHolder.push("won't you be my neighbor")
       }else if(i.toString().includes(2)) {
         stringHolder.push("*boop*")
       }else if(i.toString().includes(1)) {
