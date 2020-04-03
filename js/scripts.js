1e34eef4c59a71104a6ff1a09523b2514ff87b23
Tyler Bates <Fondlytyler@gmail.com>
2020-04-03 11:05:09 -0700

    revert back to B.D.D. to first get the input to return the count of input integer.

--- js/scripts.js
+++ js/scripts.js
@@ -1,38 +1,32 @@
 // Logic Spec's
 // Make sure input is a number (html input)
-// accomodate multiple numbers (split into array by spaces and punctuation)
+// take input integer and break into count leading up to input integer
 // apply logic for number 1 (beep)
 // apply logic for number 2 (boop)
 // apply logic for number 3 (wont you be my neighbor)
 
 // *Business Logic*
-var array = num.split(' ',',','.',':','?');
-var sounds =["*beep*","*boop*","won't you be my neighbor?"];
-  function translate(num) {
-    if(num === 0) {
-      return ""
-    }
-    for(i=0; i < array.length; i ++) {
-      if(num <=[i][0]) {
-        return sounds[i][0];
-      }
-    }
-  }
-  console.log()
-  // array.map(function(num) {
-  //   if (num >= 1){
-  //     replace("*beep*")
-  //   } else if (num === 2){
-  //     replace("*boop*")
-  //   } else if (num === 3){
-  //     replace("won't you be my neighbor?")
-  //   }
 
+
+
+  
+
+  
+  
+ 
 // *User Interface Logic*
 $(document).ready(function() {
+  var sounds =["*beep*","*boop*","won't you be my neighbor?"];
+  let numHolder = []
   $("form#numbers").submit(function(event) {
     event.preventDefault();
-  
-      var num = $("input#number").val();
+    
+    var num = $("input#number").val();
+    numHolder.push(parseInt(num))
+    if(num > 5) num = 5
+      for(let i = 0; i <= num; i++){
+        console.log(i)
+      }
+
   });
 });
