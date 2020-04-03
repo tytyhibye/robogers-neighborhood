2564b1577cd63d365ef4543ad7f0209bd25e1270
Tyler Bates <Fondlytyler@gmail.com>
2020-04-03 08:44:37 -0700

    add logic specs for website.

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+// Logic Spec's
+// Make sure input is a number (html input)
+// accomodate multiple numbers (split into array by spaces and punctuation)
+// apply logic for number 1 (beep)
+// apply logic for number 2 (boop)
+// apply logic for number 3 (wont you be my neighbor)
+
+// *Business Logic*
+
+
+
+
+// *User Interface Logic*
+$(document).ready(function() {
+  $("form#numbers").submit()
+})
