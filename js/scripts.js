f5b82656c596feff6e842faa49e2fe722f4e56f6
Tyler Bates <Fondlytyler@gmail.com>
2020-04-03 14:18:19 -0700

    further test ways to display results on webpage.

--- js/scripts.js
+++ js/scripts.js
@@ -7,35 +7,31 @@
 // apply cascade so logic order is for number 3, then 2, then 1
 // display results of logic in user interface
 
-// *Business Logic*
-function translate(num) {
-  if(num === 0){
-    return ""
-  }
-  for(i = 0; i <= num; i++) {
-    if(i.toString().includes(3)) {
-      return "Won't you be my neighbor";
-    }else if(i.toString().includes(2)) {
-      return "*boop*";
-    }else if(i.toString().includes(1)) {
-      return "*beep*";
-    }else {
-      return i;
-    }
-  }
-}
- 
-// *User Interface Logic*
 $(document).ready(function() {
+    
+  var output = ''
+  var num = $("input#number").val();
+  // if(num > 50) num = 50
+  let translate = function() {
+    for(i = 0; i <= num; i++) {
+      if(i.toString().includes(3)) {
+        console.log(i)
+        output = "Won't you be my neighbor";
+        console.log(output)
+      }else if(i.toString().includes(2)) {
+        output = "*boop*";
+      }else if(i.toString().includes(1)) {
+        output = "*beep*";
+      }else {
+        output = i;
+      }
+    }
+  }  
+  
+
   $("#numbers").submit(function(event) {
     event.preventDefault();
-      
-      var num = $("input#number").val();
-    // if(num > 50) num = 50
-      var result = translate(num);
-      // if (result) {
-      $("#output").text(result);
-      console.log('test');
-      // }
+    translate()
+    $("#output").text(output);
   });
 });
